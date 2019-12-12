c91bf4023b6edc8fd7756fda06c71339d575a360
Houlgatte Louka <bazar.tri@gmail.com>
2019-12-12 22:28:01 +0100

    Add connection from web local to api deployed

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import './App.css';
 import Routes from './component/routes/routes';
 import Header from "./component/header/header";
 import {BrowserRouter} from "react-router-dom";
+import {useKeycloak} from "react-keycloak";
+import axiosOrder from "./axios-order";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Header/>
-        <Routes/>
-      </BrowserRouter>
-    </div>
-  );
+
+    const {keycloak, initialized} = useKeycloak();
+
+    if (initialized) {
+        axiosOrder.defaults.headers.common = {'Authorization': 'Bearer ' + keycloak.token};
+    }
+
+    return initialized ? (
+        <div className="App">
+            <BrowserRouter>
+                <Header/>
+                <Routes/>
+            </BrowserRouter>
+        </div>
+    ) : null;
 }
 
 export default App;
