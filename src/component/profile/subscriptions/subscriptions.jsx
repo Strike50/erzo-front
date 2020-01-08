6618b335d75a28cf41da2ecb041df4d405dd60db
Remi- <remi-castel@hotmail.fr>
2020-01-08 10:00:38 +0100

    Afficher subs et followers

---
+++ src/component/profile/subscriptions/subscriptions.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {ListGroup, ListGroupItem, Modal, ModalBody, ModalHeader} from "reactstrap";
+import {NavLink} from "react-router-dom";
+
+
+export const Subscriptions = props => {
+    const title = props.isFollowing ? 'Abonnements': 'Abonnés';
+    return(
+        <Modal isOpen={props.isOpen} toggle={props.toggle} className="">
+            <ModalHeader toggle={props.toggle}>{title}</ModalHeader>
+            <ModalBody>
+                <ListGroup>
+                    {props.profileList.map((user, i) => (
+                        <ListGroupItem key={`userSearch-${i}`}>
+                            <NavLink to={`/profil/${user.username}`}>
+                                {user.firstName} {user.lastName} - {user.username}
+                            </NavLink>
+                        </ListGroupItem>
+                    ))}
+                </ListGroup>
+            </ModalBody>
+        </Modal>
+    );
+
+};
+
+export default Subscriptions;
+
+Subscriptions.propTypes = {
+    profileList: PropTypes.arrayOf(PropTypes.object).isRequired,
+    isOpen: PropTypes.bool.isRequired,
+    toggle: PropTypes.func.isRequired,
+    isFollowing: PropTypes.bool.isRequired,
+};
