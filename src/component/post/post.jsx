69effc1c76b778449af3be77e76b57e929379ce3
Houlgatte Louka <bazar.tri@gmail.com>
2020-01-16 20:25:47 +0100

    remove unnecessary comment

--- src/component/post/post.jsx
+++ src/component/post/post.jsx
@@ -19,7 +19,7 @@ export const Post = props => {
             });
         }
     }, [postId, getPostById]);
-    
+
     return postObject !== {} ? (
         <div>
             <PostDisplay id={postObject.id}
@@ -32,41 +32,6 @@ export const Post = props => {
             <CreatePost />
         </div>
     ) : null;
-
-    const creationDate = new Date(props.creationDate).toLocaleString();
-    return (
-        <Card className="card-post">
-            <CardHeader>
-                <div style={{float: "right", cursor: "pointer"}} onClick={onClickDeletePost}>&#x2716;</div>
-                {authorDetail}
-                <div>{`Publié le ${creationDate}`}</div>
-            </CardHeader>
-            <CardBody>
-                <Row>{props.content}</Row>
-                <Row>
-                    <Col>
-                        {checkMediaPlayer()}
-                    </Col>
-                </Row>
-            </CardBody>
-            <CardFooter>
-                <Row>
-                    <Col md="4">
-                        <FontAwesomeIcon className="icon" icon="comment"/>
-                        <span>{nbComment}</span>
-                    </Col>
-                    <Col md="4">
-                        <FontAwesomeIcon className="icon" icon="retweet" color={idRT !== null ? 'green' : null} onClick={onClickRT}/>
-                        <span>{nbRT}</span>
-                    </Col>
-                    <Col md="4">
-                        <FontAwesomeIcon className="icon" icon="heart" color={idLike !== null ? 'red' : null} onClick={onClickLike}/>
-                        <span>{nbLike}</span>
-                    </Col>
-                </Row>
-            </CardFooter>
-        </Card>
-    );
 };
 
 const mapDispatchToProps = dispatch => {
