718058b8039c68c424959d7db3a1e83ab102341e
Laplo <ronanlaplaud4@gmail.com>
2020-01-16 23:13:07 +0100

    add updateNotifications after seen them

--- src/component/notification/notifications-list/notifications-list.jsx
+++ src/component/notification/notifications-list/notifications-list.jsx
@@ -35,7 +35,6 @@ export const NotificationsList = props => {
                     notifications.forEach(notification => notification.notificationStatus = eNotificationStatus.SEEN);
                 }, 5000);
         });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     notifications.sort((a, b) => {
