99a055e963b3292f558431855d8308e44e67390f
Houlgatte Louka <lhoulgattebustamante@gmail.com>
2020-01-16 07:49:02 +0000

    Test NODE_ENV

--- src/axios-order.js
+++ src/axios-order.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const TIMEOUT = 60 * 1000;
-
+console.log(process.env.NODE_ENV);
 const baseUrl = process.env.NODE_ENV === 'production' ? 'http://api.erzo.wtf' : 'http://localhost:3001';
 const axiosOrder = axios.create({
   baseURL: baseUrl,
